30d0df12c1e73b168e28fa2a85fd9584dee99b45
Carlos Sánchez <carloschemin00@gmail.com>
2023-06-20 19:09:34 -0600

    Resolve Desing

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,16 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <React.StrictMode>
+  <head>
+   <link
+    rel="stylesheet"
+    href="https://bootswatch.com/5/flatly/bootstrap.min.css"
+   />
+   <link
+    href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+    rel="stylesheet"
+   />
+  </head>
   <Routes />
  </React.StrictMode>
 )
